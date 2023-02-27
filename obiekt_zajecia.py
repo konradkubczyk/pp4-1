108d417da9f58787e7748dc49c351bba24893690
Konrad Kubczyk <konrad@kubczyk.com>
2023-02-27 21:36:02 +0100

    Dodanie obiektu z kontrolą limitu elementów

---
+++ obiekt_zajecia.py
@@ -0,0 +1,40 @@
+class Zajecia:
+    studenci = []
+
+    def __init__(self, nazwa):
+        """Konstruktor klasy Zajecia
+        
+        Argumenty:
+        nazwa -- nazwa zajęć
+        """
+        self.nazwa = nazwa
+
+    def zapisz_studenta(self, student):
+        """Zapisuje studenta na zajęcia, przy czym nie dopuszcza do zapisu więcej niż 10 osób
+        
+        Argumenty:
+        student -- obiekt klasy Student
+        """
+        if len(self.studenci) < 10:
+            self.studenci.append(student)
+        else:
+            raise Exception("Limit studentów na zajęcia został osiągnięty, nie można zapisać studenta")
+
+# Testy ----------------------------
+
+zajecia = Zajecia("Pracownia programowania IV")
+
+zajecia.zapisz_studenta("Student A")
+zajecia.zapisz_studenta("Student B")
+zajecia.zapisz_studenta("Student C")
+zajecia.zapisz_studenta("Student D")
+zajecia.zapisz_studenta("Student E")
+zajecia.zapisz_studenta("Student F")
+zajecia.zapisz_studenta("Student G")
+zajecia.zapisz_studenta("Student H")
+zajecia.zapisz_studenta("Student I")
+zajecia.zapisz_studenta("Student J")
+
+print(zajecia.studenci) # ['Student A', 'Student B', 'Student C', 'Student D', 'Student E', 'Student F', 'Student G', 'Student H', 'Student I', 'Student J']
+
+zajecia.zapisz_studenta("Student K") # Exception: Limit studentów na zajęcia został osiągnięty, nie można zapisać studenta
