bf8d8bcc338affaec4e3396554bb275148b33c32
Konrad Kubczyk <konrad@kubczyk.com>
2023-02-27 21:20:54 +0100

    Dodanie procedury dopisującej dane do pliku

---
+++ procedura_dopisujaca_dane.py
@@ -0,0 +1,11 @@
+import os.path
+
+ciag_znakow = input("Podaj ciąg znaków: ")
+
+with open("dane.txt", "a") as plik:
+
+    # Nowa linia jest dodawana tylko gdy plik nie jest pusty, by ilość linii była równa ilości wprowadzonych ciągów znaków
+    if os.path.getsize("dane.txt") > 0:
+        plik.write("\n")
+        
+    plik.write(ciag_znakow)
