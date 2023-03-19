d8b1c7d1c0877875f2afd41f90dfbf362ead25e5
Konrad Kubczyk <konrad@kubczyk.com>
2023-03-19 23:36:28 +0100

    Adaptacja procedury do zewnętrznych testów

--- procedura_dopisujaca_dane.py
+++ procedura_dopisujaca_dane.py
@@ -1,9 +1,11 @@
 import os.path
 
-ciag_znakow = input("Podaj ciąg znaków: ")
+if __name__ == '__main__':
+    ciag_znakow = input("Podaj ciąg znaków: ")
+else:
+    ciag_znakow = "Testowy ciąg znaków"
 
 with open("dane.txt", "a") as plik:
-
     # Nowa linia jest dodawana tylko gdy plik nie jest pusty, by ilość linii była równa ilości wprowadzonych ciągów znaków
     if os.path.getsize("dane.txt") > 0:
         plik.write("\n")
